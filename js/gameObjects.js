0a04d63408e4f6ed7b3c9e8fce6faa77d8b539ba
Isvan <iavor@telus.net>
2017-05-02 17:29:32 -0700

    Added basic objects to the game

--- js/gameObjects.js
+++ js/gameObjects.js
@@ -2,7 +2,37 @@ function Base(x,y,hp){
     
     //So we can grab the position as one object
     this.pos = {x,y};
-    
     //Set hp
-    this.hp = hp
+    this.maxHp = hp;
+    this.curHp = hp;
+    
+    this.isDead = false;
+    
+}
+
+Base.prototype = {
+    
+    takeDamg : function(amount){
+        this.curHp -= amount;
+        if(this.curHp < 0){
+            this.isDead = true;
+        }
+    }
+    
+    heal : function(amount){
+        this.curHp += amount;
+        if(this.curHp > this.maxHp){
+            this.curHp = this.maxHp;
+        }
+        
+        
+    }
+    
+}
+
+function Tower(x,y){
+    
+    this.x = x;
+    this.y = y;
+    
 }
